4883c9dda6be89582fa3cb7c16ddb475ceeeacbc
Pine Mizune <pinemz@gmail.com>
2019-08-30 20:38:33 +0900

    feat: add classifiers

--- setup.py
+++ setup.py
@@ -4,8 +4,15 @@ from setuptools import find_packages, setup
 
 setup(
     name='ecode',
-    packages=find_packages("src"),
-    package_dir={"": "src"},
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     setup_requires=['pytest-runner'],
     tests_require=['pytest']
 )
