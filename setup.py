84108bdd3f190aaa96c6fb58e4e5ec8711bfd689
Pine Mizune <pinemz@gmail.com>
2019-08-30 23:48:57 +0900

    feat: add version

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 # -*- encoding: utf-8 -*-
 
+import re
+
 from setuptools import find_packages, setup
+
+
+with open('src/ecode/__init__.py', encoding='utf8') as f:
+    version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
+
 
 setup(
     name='ecode',
+    version=version,
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3 :: Only',
--- src/ecode/__init__.py
+++ src/ecode/__init__.py
@@ -2,3 +2,5 @@
 
 from .ecode_encoder import EcodeEncoder
 from .ecode_decoder import EcodeDecoder
+
+__version__ = '0.1.0'
